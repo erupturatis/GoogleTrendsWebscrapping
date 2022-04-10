1389a1851b25e495c95ecef9590a17572d0b94b4
erupturatis <bb.octav@gmail.com>
2022-04-10 19:40:09 +0300

    inital commit

---
+++ request_stock.ps1
@@ -0,0 +1,32 @@
+function get($symbol, $start, $end) {
+    # Write-Output $symbol
+    
+    $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+    $session.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+    Invoke-WebRequest -UseBasicParsing -Uri "https://api.nasdaq.com/api/quote/$symbol/chart?assetclass=stocks&fromdate=$start&todate=$end" `
+    -WebSession $session `
+    -Headers @{
+    "authority"="api.nasdaq.com"
+    "method"="GET"
+    "path"="/api/quote/$symbol/chart?assetclass=stocks&fromdate=$start&todate=$end"
+    "scheme"="https"
+    "accept"="application/json, text/plain, */*"
+    "accept-encoding"="gzip, deflate, br"
+    "accept-language"="en-US,en;q=0.9"
+    "origin"="https://www.nasdaq.com"
+    "referer"="https://www.nasdaq.com/"
+    "sec-ch-ua"="`" Not A;Brand`";v=`"99`", `"Chromium`";v=`"100`", `"Google Chrome`";v=`"100`""
+    "sec-ch-ua-mobile"="?0"
+    "sec-ch-ua-platform"="`"Windows`""
+    "sec-fetch-dest"="empty"
+    "sec-fetch-mode"="cors"
+    "sec-fetch-site"="same-site"
+    } 
+}
+ 
+
+
+$res = get $args[0] $args[1] $args[2]
+$var = $res.Content
+return $var
+
---
+++ stocks.py
@@ -0,0 +1,155 @@
+import json
+
+import subprocess
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+
+class StockTrendsRequester(object):
+
+    STOCK_URL_PART1 = 'https://api.nasdaq.com/api/quote/'
+    STOCK_URL_PART2 = '/chart'
+    SEARCH_URL = 'https://api.nasdaq.com/api/autocomplete/slookup/10'
+
+    def __init__(self) -> None:
+        pass
+
+    def verify_initialization(self):
+        # if a session is not created yet, it will make one 
+        if hasattr(self, 'session'):
+            print("session already created")
+        else:
+            self.init_requester()
+            print("creating session ")
+
+    def init_requester(self) -> None:
+        session = requests.session()
+        self.session = session
+
+    
+    def verification_needed(func):
+        #decorator to call verify_initialization on all functions where is needed 
+        def function_wrapper(*args):
+            
+            self = args[0]
+            self.verify_initialization()
+            func(*args)
+        return function_wrapper
+
+    def get_search_result(self,keyword:str) -> str:
+        '''
+        Uses the company name to get the first search result on the website 
+        This allows you to simply call the function with the company name instead
+        of the stock abbreviation
+        It uses the data for the stock fetching
+        '''
+        
+        powershell = f"powershell ./request.ps1 {keyword}"
+        response = subprocess.check_output(powershell, shell=True)
+        response = response.decode("utf-8")
+        return json.loads(response)["data"][0]["symbol"]
+       
+    def get_stock(self, symbol, days:int) -> list:
+        '''
+        makes use of another powershell file to fetch the data because 
+        python requests library doesn t work for some reason
+        '''
+        self.get_day(days=days)
+        powershell = f"powershell ./request_stock.ps1 {symbol} {self.days_ago} {self.yesterday}"
+        response = subprocess.check_output(powershell, shell=True)
+        # decodes and turn response into pytohn dictionary
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        return response["data"]["chart"]
+
+
+    def write(self,text=' ',file:str='textfile') -> None:
+        text_file = open(f'{file}.txt','w')
+        text_file.write(text)
+        text_file.close()
+
+
+    def get_day(self, days:int)->None:
+        today = datetime.today()
+        self.days_ago = str(today - timedelta(days=days))
+        self.yesterday = str(today - timedelta(days=1))
+        self.days_ago = self.days_ago[:10]
+        self.yesterday = self.yesterday[:10]
+
+
+
+    def get_stock_data(self, keyword:str='TSLA', days:int=90 ) -> list:
+        self.get_stock_flow(keyword,days)
+        return self.dataframe
+
+
+    def save_stock_data(self, keyword:str='TSLA', days:int=90 ) -> list:
+        self.get_stock_flow(keyword,days)
+        self.dataframe.to_csv(f'{keyword}.csv')
+    
+
+    def fill_blank_days(self,result:list,days:int)->list:
+        # Normalizing the data, filling the missing day spaces
+   
+        final_result = result
+        today = datetime.today()
+        days_count = days
+        
+        for i in range(0,days-1):
+            days_ago1 = str(today - timedelta(days=days_count-i))
+            days_ago1 = days_ago1[:10]
+            d1 = days_ago1.split("-")
+            days_ago1 = f"{d1[1]}/{d1[2]}/{d1[0]}"
+            #brings the dates to the same format as result date
+
+            
+            if not final_result[i]['times']==days_ago1:
+                final_result.insert(i,{
+                    'values': result[i]['values'],
+                    'times': days_ago1
+                })
+
+        
+        return final_result
+
+
+
+    @verification_needed
+    def get_stock_flow(self, keyword:str='TSLA', days:int = 90) -> None:
+        #default flow for all requests
+
+        symbol = self.get_search_result(keyword=keyword)
+        result = self.get_stock(symbol,days=days+1)
+
+        values = list()
+        times = list()
+        
+        simple_result = list()
+
+        for element in result:
+            simple_result.append({
+                'values': element["z"]["value"],
+                'times': element["z"]["dateTime"]
+            })
+
+        simple_result = self.fill_blank_days(simple_result,days=days+1)
+  
+        for element in simple_result:
+            values.append(element["values"])
+            times.append(element["times"])
+        
+        df = pd.DataFrame(
+            {
+            'values': values,
+            'times': times
+            } 
+        )
+        self.dataframe=df
+
+
+
+if __name__ == '__main__':
+
+    st = StockTrendsRequester()
+    st.save_stock_data('apple',90)
+   
